ccf40460bc7202b6c26f5c4caf224410b1a0b5a6
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-03-12 10:54:18 -0400

    Started working on the header

--- src/sharedComponents/Header/Header.Component.jsx
+++ src/sharedComponents/Header/Header.Component.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import "./Header.Component.css";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>This is a Header</h1>
+      <div className="flex-container">
+        <span className="flex-align-left">BootsBook</span>
+        <span className="flex-align-right">Search</span>
       </div>
     );
   }
