f16b37e7f33e08bac418c65818c0138e9aeb6276
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-03-12 20:39:08 -0400

    got write post mostly up and running

--- src/components/WritePost/WritePost.Component.jsx
+++ src/components/WritePost/WritePost.Component.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import "./WritePost.Component.css";
 
 export default class RightSidebar extends Component {
   render() {
     return (
-      <div className="flex-container">
-        <p>This is where you write a post</p>
+      <div className="flex-container write-container">
+        <div className="write-header">
+          <button className="selected-option">Make a publication</button>
+          <button className="not-selected-option">Images</button>
+        </div>
+        <div className="write-body">
+          <textarea
+            placeholder="What are you thinking?"
+            className="writing-textarea"
+          />
+        </div>
+        <div className="write-footer">
+          <button className="button-primary">Share</button>
+          <button className="who-can-see" title="who can see your post">
+            ?
+          </button>
+        </div>
       </div>
     );
   }
